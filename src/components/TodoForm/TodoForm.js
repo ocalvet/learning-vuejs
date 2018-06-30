1508d694650662975b01af8a4ddc81891de78e1b
Ovidio R. Calvet <calveto@gmail.com>
2018-06-29 21:06:00 -0400

    Add todos to the todo form

--- src/components/TodoForm/TodoForm.js
+++ src/components/TodoForm/TodoForm.js
@@ -1,6 +1,27 @@
 export default {
   name: 'TodoForm',
-  props: {
-    todos: Array,
-  },
+  // props: {
+  //   todos: Array,
+  // },
+  data: () => {
+    return {
+      todos: [{
+        title: "Buy milk",
+        date: new Date(),
+        completed: false,
+      },{
+        title: "Schedule a doctor appointment",
+        date: new Date(),
+        completed: false,
+      },{
+        title: "Schedule the dentist appointment",
+        date: new Date(),
+        completed: false,
+      },{
+        title: "Clean the house",
+        date: new Date(),
+        completed: false,
+      }]
+    };
+  }
 };
--- src/components/TodoForm/TodoForm.vue
+++ src/components/TodoForm/TodoForm.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
-    Todo Form
+    <h4>Todo Form</h4>
+    <div class="todo-item" v-for="(todo, i) in todos" :key="i">{{ todo.title }}</div>
   </div>
 </template>
 
 <script src="./TodoForm.js"></script>
+<style>
+.todo-item {
+  padding: 10px 20px;
+  background-color: azure;
+  margin-bottom: 5px;
+}
+.todo-item:hover {
+  background-color: bisque;
+}
+</style>
 
